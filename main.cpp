00fb356540f3a7631cf97f88337e31f8084ae439
Dylan <quantum64development@gmail.com>
2019-05-01 20:04:09 -0400

    Add some features

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include "engine.h"
+#include "ttmath/ttmath.h"
 
-constexpr int width = 160;
-constexpr int height = 100;
-constexpr int maxIterations = 500;
+#define bits_exp 1
+#define bits_base 3
 
-int calculate(int x, int y, double scale) {
-	double scaledX = (x / static_cast<double>(width)) * 4 - 2.8;
-	double scaledY = (y / static_cast<double>(height)) * 2.2 - 1;
+using namespace ttmath;
+
+constexpr int width = 220;
+constexpr int height = 140;
+constexpr int max = height > width ? height : width;
+
+const int colors[] = { COLOUR::FG_RED, COLOUR::FG_YELLOW, COLOUR::FG_GREEN, COLOUR::FG_CYAN, COLOUR::FG_DARK_CYAN, COLOUR::FG_BLUE, COLOUR::FG_DARK_BLUE, COLOUR::FG_MAGENTA, COLOUR::FG_DARK_MAGENTA };
+
+double cxs = 0;
+double cys = 0;
+
+inline int calculate(double x, double y, double scale, int maxIterations) {
+	double scaledX = (x / static_cast<double>(max)) * 4 - 2.8;
+	double scaledY = (y / static_cast<double>(max)) * 3.5 - 1;
 
 	scaledX = (scaledX / scale);
 	scaledY = (scaledY / scale);
 
-	double cx = 0.0;
-	double cy = 0.0;
+	double cx = cxs;
+	double cy = cys;
 	int iteration = 0;
 	while (cx * cx + cy * cy <= 4 && iteration < maxIterations) {
 		double xtemp = cx * cx - cy * cy + scaledX;
@@ -23,10 +34,30 @@ int calculate(int x, int y, double scale) {
 	return iteration;
 }
 
+inline int calculate(Big<bits_exp, bits_base> x, Big<bits_exp, bits_base> y, Big<bits_exp, bits_base> scale, int maxIterations) {
+	Big<bits_exp, bits_base> scaledX = (x / static_cast<double>(width)) * 4 - 2.8;
+	Big<bits_exp, bits_base> scaledY = (y / static_cast<double>(height)) * 2.2 - 1;
+
+	scaledX = (scaledX / scale);
+	scaledY = (scaledY / scale);
+
+	Big<bits_exp, bits_base> cx = 0.0;
+	Big<bits_exp, bits_base> cy = 0.0;
+	int iteration = 0;
+	while (cx * cx + cy * cy <= 4 && iteration < maxIterations) {
+		Big<bits_exp, bits_base> xtemp = cx * cx - cy * cy + scaledX;
+		cy = cx * cy * 2 + scaledY;
+		cx = xtemp;
+		iteration = iteration + 1;
+	}
+	return iteration;
+}
+
 class Fractal : public olcConsoleGameEngine {
 private:
-	double xOffset = 0, yOffset = 0;
-	double scale = 1;
+	Big<bits_exp, bits_base> one = "1";
+	Big<bits_exp, bits_base> xOffset = 0, yOffset = 0;
+	Big<bits_exp, bits_base> scale = 1;
 
 public:
 	bool OnUserCreate() {
@@ -36,52 +67,89 @@ public:
 	bool OnUserUpdate(float delta) {
 		Fill(0, 0, width, height, COLOUR::FG_BLACK);
 		if (m_keys[87].bHeld) {
-			yOffset -= 150 * delta / scale;
+			yOffset -= (one / scale) * 150 * delta;
 		}
 		if (m_keys[83].bHeld) {
-			yOffset += 150 * delta / scale;
+			yOffset += (one / scale) * 150 * delta;
 		}
 		if (m_keys[68].bHeld) {
-			xOffset += 150 * delta / scale;
+			xOffset += (one / scale) * 150 * delta;
 		}
 		if (m_keys[65].bHeld) {
-			xOffset -= 150 * delta / scale;
+			xOffset -= (one / scale) * 150 * delta;
 		}
 		if (m_keys[69].bHeld) {
-			scale += 4 * delta * scale;
+			scale += scale * 4 * delta;
 		}
 		if (m_keys[81].bHeld) {
-			if (scale > 0) {
-				scale -= 4 * delta * scale;
-			}
+			scale -= scale * 4 * delta;
 		}
+		if (m_keys[69].bHeld) {
+			scale += scale * 4 * delta;
+		}
+		if (m_keys[81].bHeld) {
+			scale -= scale * 4 * delta;
+		}
+
+		if (m_keys[90].bHeld) {
+			cys += delta / 2;
+		}
+		if (m_keys[88].bHeld) {
+			cys -= delta / 2;
+		}
+		if (m_keys[67].bHeld) {
+			cxs += delta / 2;
+		}
+		if (m_keys[86].bHeld) {
+			cxs -= delta / 2;
+		}
+
+		Big<bits_exp, bits_base> tmp = scale * scale;
+		tmp.Ln(tmp);
+		int maxIterations = 200 + 10 * static_cast<int>(tmp.ToDouble());
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				int iterations = calculate(x + static_cast<int>(xOffset * scale), y + static_cast<int>(yOffset * scale), scale);
+				int iterations = 0;
+				//if (scale < 80e13) {
+					iterations = calculate((scale * xOffset + x).ToDouble(), (scale * yOffset + y).ToDouble(), scale.ToDouble(), maxIterations);
+				//}
+				//else {
+					//iterations = calculate(scale * xOffset + x, scale * yOffset + y, scale, maxIterations);
+				//}
+				int color = colors[iterations / 10 % 9];
+				if (iterations == maxIterations) {
+					color = COLOUR::FG_DARK_RED;
+				}
+				if (iterations < 5) {
+					color = COLOUR::FG_BLACK;
+				}
+				Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, color);
+				/*
 				if (iterations == maxIterations) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_DARK_RED);
 				}
-				else if (iterations > 100) {
+				else if (iterations > (maxIterations / 2)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_RED);
 				}
-				else if (iterations > 75) {
+				else if (iterations > (maxIterations / 3)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_YELLOW);
 				}
-				else if (iterations > 50) {
+				else if (iterations > (maxIterations / 5)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_GREEN);
 				}
-				else if (iterations > 25) {
+				else if (iterations > (maxIterations / 10)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_BLUE);
 				}
-				else if (iterations > 12) {
+				else if (iterations > (maxIterations / 30)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_DARK_BLUE);
 				}
-				else if (iterations > 5) {
+				else if (iterations > (maxIterations / 50)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_MAGENTA);
 				}
-				else if (iterations > 3) {
+				else if (iterations > (maxIterations / 75)) {
 					Draw(x, y, PIXEL_TYPE::PIXEL_SOLID, COLOUR::FG_DARK_MAGENTA);
 				}
+				*/
 			}
 		}
 		return true;
@@ -91,7 +159,7 @@ public:
 int main()
 {
 	Fractal game;
-	game.ConstructConsole(160, 100, 8, 8);
+	game.ConstructConsole(width, height, 8, 8);
 	game.Start();
 	return 0;
 }
